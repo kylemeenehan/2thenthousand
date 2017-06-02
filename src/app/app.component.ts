d2b6f518b86db77633de4c7843c46883d1006867
Kyle Meenehan <kyle.meenehan@hellocomputer.com>
2017-06-02 15:32:31 +0200

    initialized canvas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('canvas') canvas;
+  ctx;
+
+  ngAfterViewInit(){
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+  }
 }
